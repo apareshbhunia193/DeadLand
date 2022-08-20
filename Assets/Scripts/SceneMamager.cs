f521eb7daca74f8272efd4bb1e87eeb388bd230c
Aparesh Bhunia <apareshbhunia@gmail.com>
2022-08-21 03:18:49 +0530

    Bug Fix, Final Touch

---
+++ Assets/Scripts/SceneMamager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMamager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPlayButton() {
+        SceneManager.LoadScene("GameLevel");
+    }
+
+    public void OnClickExitButton() {
+        Application.Quit();
+    }
+
+    public void OnClickMainMenuButton() {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
